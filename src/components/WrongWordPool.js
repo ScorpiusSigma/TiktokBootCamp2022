7867b6fc6023ad13f878de590d315d1564952fc1
Justin <justinleng_@hotmail.com>
2022-05-22 19:14:53 +0800

    [Edit] Wrong Word Pool height

--- src/components/WrongWordPool.js
+++ src/components/WrongWordPool.js
@@ -4,7 +4,7 @@ function WrongWordPool(props) {
   return (
     <div className="flex flex-col items-center w-full h-full gap-5 py-3 px-6">
       <h1 className="text-2xl md:text-5xl">Wrong Word Pool</h1>
-      <div className="flex flex-row flex-wrap border rounded-xl w-[60%] min-h-16 md:min-h-20 gap-2 p-4 text-red-500 text-xl md:text-4xl">
+      <div className="flex flex-row flex-wrap border rounded-xl w-[60%] min-h-[64px] md:min-h-[80px] gap-2 p-4 text-red-500 text-xl md:text-4xl">
         {data.map((char, index) => (
           <div key={index}>{char}</div>
         ))}
